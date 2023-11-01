3b9ca9333b1e7eaab515d2834c87b6bde673c39a
William Chen <william.chen@mymail.champlain.edu>
2023-11-01 14:01:22 -0400

    Week 10 Get Service Logs Assignment Completed

---
+++ Week10/GetServiceLogs.ps1
@@ -0,0 +1,37 @@
+﻿function serviceLogs
+{
+    $serviceStatus = @('All', 'Stopped', 'Running')
+    $input = Read-Host -Prompt "Do you want to see 
+    [1] All services
+    [2] Stopped services
+    [3] Running services
+    [4] Quit
+    "
+     if ($input -eq 1)
+     {
+        Get-Service
+        serviceLogs
+     }
+
+     if ($input -eq 2)
+    {
+       Get-Service | Where-Object { $_.Status -eq $serviceStatus[1] }
+       serviceLogs
+    }
+
+     if ($input -eq 3)
+    {
+       Get-Service | Where-Object { $_.Status -eq $serviceStatus[2] }
+       serviceLogs
+    }
+    if ($input -eq 4)
+    {
+        exit
+    }
+    else
+    {
+       Write-Host "Invalid input, must be between 1-4"
+       serviceLogs
+    }
+}
+serviceLogs
