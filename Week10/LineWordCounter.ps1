775517f44b20b6c0e42af7de8eb167a05d477600
William Chen <william.chen@mymail.champlain.edu>
2023-11-08 12:46:15 -0500

    Week 10 Line/Word Counter Assignment Completed

---
+++ Week10/LineWordCounter.ps1
@@ -0,0 +1,8 @@
+﻿cd "C:\Users\champuser\Desktop\CSI-230-01"
+
+$filesToLookAt = Get-ChildItem -Recurse -Filter *.bash
+for ($i=0; $i -lt $filesToLookAt.Length; $i++)
+{
+   Get-Content $filesToLookAt[$i].FullName | Measure-Object -Word -Line
+}
+Get-ChildItem | Measure-Object -Property length -Minimum -Maximum -Sum -Average | Out-String
